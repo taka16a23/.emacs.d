5d049a4dd4478ee329ef7f4a54c5ec81885d49a3
Atami <taka16a23@gmail.com>
2015-10-02 11:03:10 +0900

    Fixed show cadidate path

--- elisp_compileall.py
+++ elisp_compileall.py
@@ -79,6 +79,9 @@ class ElispPath(object):
     def __str__(self):
         return self._path
 
+    def __repr__(self):
+        return self._path
+
 
 def iter_el_files(directory):
     r"""SUMMARY
@@ -130,16 +133,6 @@ def _main():
         files.append(str(initpath))
     if files:
         subprocess.Popen(commands + files)
-
-    # loadfile = os.path.expanduser(
-    #     '~/.emacs.d/elisp/start.d/subroutines_start.el')
-    # cmdbase = 'emacs -Q --batch -l {0} --funcall batch-byte-compile '.format(
-        # loadfile)
-    # for dir_, _, _ in os.walk('/root/.emacs.d/elisp'):
-    #     if '.svn' in dir_:
-    #         continue
-    #     args = os.path.join(dir_, '*.el',)
-    #     os.system(cmdbase + args)
     return 0
 
 if __name__ == '__main__':
