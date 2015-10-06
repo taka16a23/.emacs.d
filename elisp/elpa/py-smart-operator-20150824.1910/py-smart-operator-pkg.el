285ae7117a5e61d51898a6cc2ebaeaeeb990cb7f
Atami <taka16a23@gmail.com>
2015-10-06 13:58:12 +0900

    Removed smart-operator

--- elisp/elpa/py-smart-operator-20150824.1910/py-smart-operator-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "py-smart-operator" "20150824.1910" "smart-operator for python-mode" '((s "1.9.0")) :keywords '("python" "convenience" "smart-operator"))
