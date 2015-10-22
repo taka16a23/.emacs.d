ea3a066b11891ff6e7a04101390a449a049dd746
Atami <taka16a23@gmail.com>
2015-10-22 13:14:19 +0900

    Deleted var.d

--- var.d/junk/2015-10-04-222917.junk.el
+++
@@ -1,7 +0,0 @@
-s;; -*- coding: utf-8; mode: emacs-lisp; no-byte-compile: t; no-update-autoloads: t -*-
-;;; 2015-10-04-222917.junk.el ---
-
-(defun hello (lj) ;[2015/10/04]
-  "LJ"
-  (interactive "*")
-  )
--- var.d/junk/2015-10-12-162739.junk.el
+++
@@ -1,2 +0,0 @@
-;; -*- coding: utf-8; mode: emacs-lisp; no-byte-compile: t; no-update-autoloads: t -*-
-;;; 2015-10-12-162739.junk.el ---
