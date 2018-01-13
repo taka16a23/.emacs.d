b1a10c1aaf4e71ddd1112cefc502f9cc41676f77
Atami <taka16a23@gmail.com>
2018-01-14 01:31:54 +0900

    Change hook to auto-highlight on biz started

--- inits/56-biz-mode-setup.el
+++ inits/56-biz-mode-setup.el
@@ -6,7 +6,7 @@
 ;; Maintainer:
 ;; Version:
 ;; Created: 2017/12/31 16:57:09 (+0900)
-;; Last-Updated:2017/12/31 18:32:49 (+0900)
+;; Last-Updated:2018/01/14 01:06:10 (+0900)
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -45,14 +45,16 @@
   (require 'use-package "use-package" 'noerr))
 
 (use-package biz-mode
-             ;; :disabled
-             :defer
-             :mode (("\\.crs\\'" . biz-mode))
-             :init
-             (message "Loading \"biz\"")
-             (require 'biz-mode "biz-mode" 'noerr)
-             (add-hook 'biz-mode-hook 'yas/minor-mode 'append)
-             )
+  ;; :disabled
+  :defer
+  :mode (("\\.crs\\'" . biz-mode))
+  :init
+  (message "Loading \"biz\"")
+  (require 'biz-mode "biz-mode" 'noerr)
+  (add-hook 'biz-mode-hook 'yas/minor-mode 'append)
+  (add-hook 'biz-mode-hook 'auto-complete-mode 'append)
+  (add-hook 'biz-mode-hook 'auto-highlight-symbol-mode 'append)
+  )
 
 
 
