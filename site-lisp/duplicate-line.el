33177ab9c50a0cbb26a7c40e7edcdc96abcb7e3c
taka16a23 <takahiroatsumi0517@gmail.com>
2021-06-29 03:39:08 +0900

    Remove old style

--- site-lisp/duplicate-line.el
+++ site-lisp/duplicate-line.el
@@ -24,25 +24,21 @@
 ;; Here's a couple of commands I find indispensible;; they might as well
 ;; see the light of day now, while I'm thinking of them.  I bind them
 ;; thus:
-;; 
+;;
 ;; (global-set-key "\M-p" 'duplicate-previous-line)
 ;; (global-set-key "\M-n" 'duplicate-following-line)
-;; 
+;;
 ;; and they behave in ordinary text buffers something like the same keys
 ;; do in buffers with history, duplicating lines from nearby in the
 ;; buffer.
-;; 
+;;
 ;; I included my `point-at' macro rather than expanding it out by hand.
-;; 
+;;
 ;; Copyright notice included even though I doubt it's needed on something
 ;; so small.
 
 ;;; Code:
 
-(defmacro point-at (&rest forms)
-  "Evaluate list of forms, and return the value of point after evaluation."
-  (` (progn (,@ forms)
-	    (,@ '((point))))))
 
 (defun duplicate-line (n dir)
   "Copy the Nth following line to point.
@@ -50,17 +46,17 @@ If the last command was a duplication, replace the current line by the next
 line in direction DIR."
   (if (eq (car-safe last-command) 'duplicate-line)
       (progn
-        (delete-region (point-at (beginning-of-line nil))
-                       (point-at (end-of-line nil)))
+        (delete-region (progn (beginning-of-line nil) (point))
+                       (progin (end-of-line nil) (point)))
         (setq n (+ dir (cdr last-command))))
-    (kill-region (point-at (beginning-of-line nil))
-                 (point-at (end-of-line nil))))
+    (kill-region (progn (beginning-of-line nil) (point))
+                 (progn (end-of-line nil) (point))))
   (if (= n 0)
       (insert (current-kill 0))
     (insert (save-excursion
               (beginning-of-line (1+ n))
               (buffer-substring (point)
-                                (point-at (end-of-line nil)))))
+                                (progn (end-of-line nil) (point)))))
     (setq this-command (cons 'duplicate-line n))))
 
 (defun duplicate-previous-line (n)
